55c61a4968b2679e035cf95662ac7d018b33ba4c
SixAnChen <sixanchen1314@gmail.com>
2025-01-01 20:44:36 +0800

    Initial commit

---
+++ src/main/java/com/yupi/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.PostThumb;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+}
+
+
+
+
